48ec56b0773f0a4c087e4ba8d86f013ce4d42aae
brandon jonathan <brandonjonathan7320@gmail.com>
2023-01-30 22:43:28 +0700

    Vigenere

---
+++ vigenere.py
@@ -0,0 +1,15 @@
+def vigenere_encrypt(plaintext, kunci_asli):
+    result = []
+    for i in range(len(plaintext)):
+        x = ord(plaintext[i]) - 65
+        y = ord(kunci_asli[i]) - 65
+        result.append(chr((x + y) % 26 + 65))
+    return(result)
+
+def vigenere_decrypt(ciphertext, kunci_asli):
+    result = []
+    for i in range(len(ciphertext)):
+        x = ord(ciphertext[i]) - 65
+        y = ord(kunci_asli[i]) - 65
+        result.append(chr((x - y) % 26 + 65))
+    return(result)
