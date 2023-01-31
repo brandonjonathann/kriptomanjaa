4a868489e9fe8631976837a65c38215f55ba4013
brandon jonathan <brandonjonathan7320@gmail.com>
2023-01-31 23:18:14 +0700

    Revisi One Time Pad

--- one_time_pad.py
+++ one_time_pad.py
@@ -1,16 +1,19 @@
-# CONTOH DARI PPT
-# plaintext = "NANTIMALAMSAYATUNGGUKAMUDIDEPANWARUNGKOPI"
-# kunci = "GTRSKNCVBRWPOATQLJFMXTRPJSRZOLFHTBMAEDPVY"
-# ciphertext = "TTELSZCGBDOPMAMKYPLGHTDJMAUDDLSDTSGNKNDKG"
+import random
+
+def generate_kunci(plaintext):
+    result = []
+    for i in range(len(plaintext)):
+        result.append(chr(random.randint(65, 90)))
+    return(result)
 
 def one_time_pad_encrypt(plaintext, kunci):
     result = []
     for i in range(len(plaintext)):
-        result.append(chr(((ord(plaintext[i]) - 65) + (ord(kunci[i]) - 65)) % 26 + 65))
+        result.append(chr(((int(ord(plaintext[i])) - 65) + (int(ord(kunci[i])) - 65)) % 26 + 65))
     return(result)
 
 def one_time_pad_decrypt(ciphertext, kunci):
     result = []
     for i in range(len(ciphertext)):
-        result.append(chr(((ord(ciphertext[i]) - 65) - (ord(kunci[i]) - 65)) % 26 + 65))
+        result.append(chr(((int(ord(ciphertext[i])) - 65) - (int(ord(kunci[i])) - 65)) % 26 + 65))
     return(result)
