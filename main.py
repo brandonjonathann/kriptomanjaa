a48131f9503f9a72a39ca9adcfbef798e9b8f369
brandon jonathan <brandonjonathan7320@gmail.com>
2023-01-31 22:42:19 +0700

    Revisi

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from vigenere import *
 from extended_vigenere import *
+from playfair import *
 
 def check_alphabet(text):
     checked = []
@@ -15,6 +16,7 @@ def create_kunci(plaintext, kunci):
         kunci_asli.append(kunci[i % len(kunci)])
     return(kunci_asli)
 
+cipher = input("Pilih cipher: ")
 kunci_awal = input("Masukkan kunci: ")
 plaintext_awal = input("Masukkan plaintext: ")
 
@@ -24,13 +26,16 @@ kunci = check_alphabet(kunci_tmp)
 plaintext_tmp = [x for x in plaintext_awal]
 plaintext = check_alphabet(plaintext_tmp)
 
-kunci_asli = create_kunci(plaintext, kunci)
-kunci_extended = create_kunci(plaintext_tmp, kunci_tmp)
-
-# Vigenere Cipher
-print(vigenere_encrypt(plaintext, kunci_asli))
-print(vigenere_decrypt(vigenere_encrypt(plaintext, kunci_asli), kunci_asli))
-
-# Extended Vigenere Cipher
-print(extended_vigenere_encrypt(plaintext_tmp, kunci_extended))
-print(extended_vigenere_decrypt(extended_vigenere_encrypt(plaintext_tmp, kunci_extended), kunci_extended))
+if (cipher == "VIGENERE"):
+    kunci = create_kunci(plaintext, kunci)
+    print(vigenere_encrypt(plaintext, kunci))
+    print(vigenere_decrypt(vigenere_encrypt(plaintext, kunci), kunci))
+elif (cipher == "EXTENDED VIGENERE"):
+    kunci = create_kunci(plaintext_tmp, kunci_tmp)
+    print(extended_vigenere_encrypt(plaintext_tmp, kunci))
+    print(extended_vigenere_decrypt(extended_vigenere_encrypt(plaintext_tmp, kunci), kunci))
+elif (cipher == "PLAYFAIR"):
+    kunci = create_kunci_playfair(kunci)
+    plaintext = create_plaintext_playfair(plaintext)
+    print(playfair_encrypt(plaintext, kunci))
+    print(playfair_decrypt(playfair_encrypt(plaintext, kunci), kunci))
