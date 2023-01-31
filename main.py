d2eed839b24fe6a89f355a1cdef0641f45039ea2
brandon jonathan <brandonjonathan7320@gmail.com>
2023-01-31 23:18:21 +0700

    Revisi

--- main.py
+++ main.py
@@ -18,28 +18,30 @@ def create_kunci(plaintext, kunci):
     return(kunci_asli)
 
 cipher = input("Pilih cipher: ")
-kunci_awal = input("Masukkan kunci: ")
 plaintext_awal = input("Masukkan plaintext: ")
-
-kunci_tmp = [x for x in kunci_awal]
-kunci = check_alphabet(kunci_tmp)
-
 plaintext_tmp = [x for x in plaintext_awal]
 plaintext = check_alphabet(plaintext_tmp)
 
-if (cipher == "VIGENERE"):
-    kunci = create_kunci(plaintext, kunci)
-    print(vigenere_encrypt(plaintext, kunci))
-    print(vigenere_decrypt(vigenere_encrypt(plaintext, kunci), kunci))
-elif (cipher == "EXTENDED VIGENERE"):
-    kunci = create_kunci(plaintext_tmp, kunci_tmp)
-    print(extended_vigenere_encrypt(plaintext_tmp, kunci))
-    print(extended_vigenere_decrypt(extended_vigenere_encrypt(plaintext_tmp, kunci), kunci))
-elif (cipher == "PLAYFAIR"):
-    kunci = create_kunci_playfair(kunci)
-    plaintext = create_plaintext_playfair(plaintext)
-    print(playfair_encrypt(plaintext, kunci))
-    print(playfair_decrypt(playfair_encrypt(plaintext, kunci), kunci))
-elif (cipher == "ONE TIME PAD"):
+if (cipher == "ONE TIME PAD"):
+    kunci = generate_kunci(plaintext)
     print(one_time_pad_encrypt(plaintext, kunci))
     print(one_time_pad_decrypt(one_time_pad_encrypt(plaintext, kunci), kunci))
+
+else: 
+    kunci_awal = input("Masukkan kunci: ")
+    kunci_tmp = [x for x in kunci_awal]
+    kunci = check_alphabet(kunci_tmp)
+
+    if (cipher == "VIGENERE"):
+        kunci = create_kunci(plaintext, kunci)
+        print(vigenere_encrypt(plaintext, kunci))
+        print(vigenere_decrypt(vigenere_encrypt(plaintext, kunci), kunci))
+    elif (cipher == "EXTENDED VIGENERE"):
+        kunci = create_kunci(plaintext_tmp, kunci_tmp)
+        print(extended_vigenere_encrypt(plaintext_tmp, kunci))
+        print(extended_vigenere_decrypt(extended_vigenere_encrypt(plaintext_tmp, kunci), kunci))
+    elif (cipher == "PLAYFAIR"):
+        kunci = create_kunci_playfair(kunci)
+        plaintext = create_plaintext_playfair(plaintext)
+        print(playfair_encrypt(plaintext, kunci))
+        print(playfair_decrypt(playfair_encrypt(plaintext, kunci), kunci))
